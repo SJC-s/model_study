4a1a33b950b9c74c70068bbab01ab9e92afd34eb
unknown <sssm4719@gmail.com>
2024-12-29 17:05:09 +0900

    proj1-fastapi model setting

--- proj1/cls_api_backup.py
+++ proj1/cls_api_backup.py
@@ -18,16 +18,25 @@ import numpy as np
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile):
     # STEP 3: Load the input image.
+    # open file -> binary
+    # http file -> text -> binary 변환 필요
     contents = await file.read()
     nparr = np.fromstring(contents, np.uint8)  # 변환
+
+    # 공식 가이드 :  imread로 변환
+    # cv_mat = cv2.imread(input_file) # file open + image decode 결합되어 있음
     cv_mat = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
     image = mp.Image(image_format=mp.ImageFormat.SRGB, data=cv_mat)
+
+    # mp.Image : 추론을 위한 별도 객체
+    # image = mp.Image.create_from_file('burger.jpg')  
 
     # STEP 4: Classify the input image.
     classification_result = classifier.classify(image) # forward(), inference(), get() 추론 이름
 
     # STEP 5: Process the classification result. In this case, visualize it.
     top_category = classification_result.classifications[0].categories[0]
-    result = f"{top_category.category_name} ({top_category.score:.2f})"
+    print(f"{top_category.category_name} ({top_category.score:.2f})")
 
-    return {"result" : result}
+    return {"filename": file.filename,
+            "filesize": len(contents)}
